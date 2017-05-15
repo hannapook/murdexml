bc5e29c614125fefc09cea0e5303d503650777e7
Hanna Pook <hanna.pook@gmail.com>
2017-05-15 22:56:43 +0300

    parandatud (ülle soovitused)

--- tsukkel3.py
+++ tsukkel3.py
@@ -1,9 +1,13 @@
 import xml.etree.ElementTree as ET
 import re
 from xml.sax.saxutils import escape
-snr=ET.Element("sr")
+snr=ET.Element("x:sr")
 
-f = open('08.xml.eelex.txt', 'r', encoding='utf8')
+snr.attrib['xmlns:x']="http://www.eki.ee/dict/hem"
+snr.attrib['xml:lang']="et"
+
+f = open('08.xml.eelex.txt', 'r', encoding='utf-8')
+#ülle tahab, et kodeering oleks utf-8, aga lõppfailis on ikkagi utf8
 a=f.readlines()
 b=[i.strip() for i in a]
 def listiks(alglist):
@@ -43,10 +47,11 @@ def listiks(alglist):
         else:
         #lisab tühja rea (¤) ja liitsõnade esimese osa enne liitsõnade teist osa    
             try:
-                if "	<w:t>#NBH#</w:t>" in alglist[i+1]:
+                if "	<w:t>#NBH#</w:t>" in alglist[i+1] or ", </w:t><w:t>#NBH#</w:t>" in alglist[i+1]:
                     c.append(j)
                     c.append("¤")
                     c.append(msrida)
+                        
                 else:
                     c.append(j)
             except IndexError:
@@ -58,18 +63,17 @@ def listiks(alglist):
 d=listiks(b)
 
 
-#for i in d[10000:]:
+#for i in d:
 #   print(i)
 
 #kustutab ära tühjad märgendid
 #for i in d:
 #    uus=re.sub('\<w:t\>[ ]*\<\/w:t\>','',i)
 
-sisutekst=""
 def xmliks(snr, rida):
     i=rida+1
-    sisutekst=""
-    art_viide=""
+    kommentaar=""
+    th_viide=""
     while not d[i].startswith("¤"):
         if d[i].startswith('<w:rStyle w:val="ms1"') or (d[i].startswith('<w:rStyle w:val="PoolpaksKiri"/>') and d[i-1].startswith("¤")):
             marksona_ise=re.search('(?<=\<w:t\>)\*{,1}[\w|-]+', d[i])
@@ -79,12 +83,22 @@ def xmliks(snr, rida):
             ms_grupp = ET.SubElement(pais, "x:mg")
             marksona = ET.SubElement(ms_grupp, "x:m")
             marksona.text=marksona_ise.group(0)
+            
         #liitsõna lisamine märksõnale
-        elif ("<w:t>|" in d[i] or "	<w:t>#NBH#</w:t>" in d[i]) and marksona.text!="":
+        elif ("<w:t>|" in d[i] or "	<w:t>#NBH#</w:t>" in d[i] or ", </w:t><w:t>#NBH#</w:t>" in d[i]) and marksona.text!="":
             marksona.text=marksona.text+"|"
+            
         elif (d[i].startswith('<w:rStyle w:val="ms2"') or d[i].startswith('<w:rStyle w:val="ms4"')) and marksona.text.endswith("|"):
             liits=re.search('(?<=\<w:t\>)\w+', d[i])
             marksona.text=marksona.text+liits.group(0)
+
+        #atribuutide lisamine märksõnale
+        elif d[i].startswith('<w:rStyle w:val="ms3"'):
+            ms_attrib=re.search('(?<=\<w:t\>)[0-9]{1,2}', d[i])
+            if not ms_attrib==None:
+                marksona.attrib['x:i']=ms_attrib.group(0)
+                marksona.attrib['x:O']=marksona.text+ms_attrib.group(0)
+
         #märksõnaviite lisamine
         elif '→' in d[i]:
             msviide=re.search('(?<=\<w:t\>→\<\/w:t\>\<w:t\>)[\w|\s|]+-{0,1}', d[i])
@@ -94,39 +108,62 @@ def xmliks(snr, rida):
                 mvtg = ET.SubElement(pais, "x:mvtg")
                 mvt = ET.SubElement(mvtg, "x:mvt")
                 mvt.text=(msviide.group(0)).strip()
-        #atribuutide lisamine märksõnale
-        elif d[i].startswith('<w:rStyle w:val="ms3"'):
-            ms_attrib=re.search('(?<=\<w:t\>)[0-9]{1,2}', d[i])
-            if not ms_attrib==None:
-                marksona.attrib['x:i']=ms_attrib.group(0)
-                marksona.attrib['x:O']=marksona.text+ms_attrib.group(0)
+                mvt.attrib['x:mvtl']="var"
+
+        #atribuutide lisamine märksõnaviitele
+        elif 'superscript' in d[i] and '→' in d[i-1]:
+            mvt_attrib=re.search('(?<=\<w:t\>)[0-9]{1,2}', d[i])
+            mvt.attrib['x:i']=mvt_attrib.group(0)
+            
+
+        #tähendusviidete lisamine
         elif 'Vrd' in d[i]:
             #töötab hetkel kui on ainult üks tähendusviide
             #mitme viite vahel on komad või märgendid, aga kuidas neid ka ikka arvestada saaks?
-            art_viide=re.search('(?<=\<w:t\>)[\w|\s]+', d[i+1])
-            if not art_viide==None:
-                art_viide=art_viide.group(0)
-                               
-          
+            #hetkel jätab esimese tähendusviite ka kommentaari osasse kui neid on rohkem kui üks
+            th_viide=re.search('(?<=\<w:t\>)[\w|\s]+', d[i+1])
+            if not th_viide==None:
+                th_viide=(th_viide.group(0)).strip()
+                sisu = ET.SubElement(artikkel, "x:S")
+                rnrp = ET.SubElement(sisu, "x:rp")
+                thnrp = ET.SubElement(rnrp, "x:tp")
+                thnr_vg=ET.SubElement(thnrp, "x:ptvtg")
+                thv=ET.SubElement(thnr_vg, "x:tvt")
+                thv.text=th_viide
+                thv.attrib['x:tvtl']="syn"
+
+        #atribuutide lisamine tähendusviitele
+        elif 'superscript' in d[i] and 'Vrd' in d[i-2]:
+            thv_attrib=re.search('(?<=\<w:t\>)[0-9]{1,2}', d[i])
+            thv.attrib['x:i']=thv_attrib.group(0)
+        
+        #ignoreerib tähendusviite ridu, st ei pane neid muu kommentaari hulka
+        #juhul kui tähendusviite real on ainult üks viide
+        elif not ',' in d[i] and 'Vrd' in d[i-1]:
+            pass
+               
         #liidab kogu ülejäänud artikli sisu kokku ja kustutab märgendid
         else:
-            sisutekst=sisutekst+re.sub('\<[\/]*w[^\>]*\>', '', d[i])
-#            print(sisutekst)
+            #juhul kui tähendusviite real on rohkem kui üks viide, siis kustutab praegu ära esimese,
+            #mis on juba tähendusviite elemendis, aga jätab ülejäänud muu kommentaari hulka
+            if ',' in d[i] and 'Vrd' in d[i-1]:
+                d[i]=re.sub('(\<w:t\>)[^,]*,(.*)$','\g<1>\g<2>', d[i])
+                kommentaar=kommentaar+re.sub('\<[\/]*w[^\>]*\>', '', d[i])
+            else:
+                kommentaar=kommentaar+re.sub('\<[\/]*w[^\>]*\>', '', d[i])
 
         i=i+1
 
-    sisu = ET.SubElement(artikkel, "x:S")
-    rnrp = ET.SubElement(sisu, "x:rp")
-    muu = ET.SubElement(rnrp, "x:km")
-    sisutekst=re.sub('#NBH#', '-', sisutekst)
-    muu.text=sisutekst
-    sisutekst=""
-    avg = ET.SubElement(rnrp, "x:atvtg")
-    aviide = ET.SubElement(avg, "x:tvt")
-    aviide.text=art_viide
+
+    if not kommentaar=="":
+        data=ET.SubElement(artikkel, "x:data")
+        kommentaar=re.sub('#NBH#', '-', kommentaar)
+        data.text=kommentaar
 
     return snr
 
+#ideaalne oleks kui saaks teha kuidagi artiklid kaheks, need kus on tähendusgrupid ja need kus ei ole
+#siis saaks panna paika nii tähendusgrupid kui ka näited
 
 for j,i in enumerate(d):
     if i.startswith("¤"):
